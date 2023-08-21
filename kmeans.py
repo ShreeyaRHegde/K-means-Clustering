d8a604c29f57af14bc668bd7c9ce53d60aa7ae9b
ShreeyaRHegde <100404757+ShreeyaRHegde@users.noreply.github.com>
2023-08-21 22:46:55 +0530

    Create kmeans.py

---
+++ kmeans.py
@@ -0,0 +1,20 @@
+#initialize kmeans parameters
+kmeans_kwargs = {
+"init": "random",
+"n_init": 10,
+"random_state": 1,
+}
+
+#create list to hold SSE values for each k
+sse = []
+for k in range(1, 11):
+    kmeans = KMeans(n_clusters=k, **kmeans_kwargs)
+    kmeans.fit(scaled_df)
+    sse.append(kmeans.inertia_)
+
+#visualize results
+plt.plot(range(1, 11), sse)
+plt.xticks(range(1, 11))
+plt.xlabel("Number of Clusters")
+plt.ylabel("SSE")
+plt.show()
